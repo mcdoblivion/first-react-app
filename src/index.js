6e6f408a81ce01f335b6cb18a4e2906935efe726
mcd_oblivion <mcd.oblivion@gmail.com>
2021-06-20 22:46:11 +0700

    Key prop and spread operator

--- src/index.js
+++ src/index.js
@@ -7,16 +7,19 @@ import './index.css';
 // Set up vars
 const books = [
   {
+    id: 1,
     title: 'Atomic Habits: An Easy & Proven Way to…',
     author: 'James Clear',
     img: 'https://images-na.ssl-images-amazon.com/images/I/713mzPe9SwS._AC_UL200_SR200,200_.jpg',
   },
   {
+    id: 2,
     title: 'Where the Crawdads Sing',
     author: 'Delia Owens',
     img: 'https://images-na.ssl-images-amazon.com/images/I/81255VTWssL._AC_UL200_SR200,200_.jpg',
   },
   {
+    id: 3,
     title: 'Joshua Weissman: An Unapologetic Cookbook',
     author: 'Joshua Weissman',
     img: 'https://images-na.ssl-images-amazon.com/images/I/91ll975iVRS._AC_UL200_SR200,200_.jpg',
@@ -28,20 +31,18 @@ function BookList() {
   return (
     <section className="book-list">
       {books.map((book) => {
-        return <Book book={book} />;
+        return <Book key={book.id} {...book} />;
       })}
     </section>
   );
 }
 
-const Book = (props) => {
-  const { img, title, author } = props.book;
+const Book = ({ img, title, author }) => {
   return (
     <article className="book">
       <img src={img} alt="" />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {props.children}
     </article>
   );
 };
